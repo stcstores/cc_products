cc0cd48e7e7b9305a6967fb863780e3af14b7892
Luke Shiner <luke@lukeshiner.com>
2018-02-28 10:56:10 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name='cc_products',
+    version='1.0',
+    description='Cloud Commerce product management',
+    author='Luke Shiner',
+    author_email='luke@lukeshiner.com',
+    url='http://github.com/stcstores/cc_products',
+    keywords=['table', 'csv', 'simple'],
+    install_requires=[],
+    packages=find_packages(),
+    )
