4f9cf57e8f996e3cbb736fa6c7e0654fddf0e694
Luke Shiner <luke@lukeshiner.com>
2018-08-01 16:02:38 +0100

    Blacken the Everything.

--- cc_products/__version__.py
+++ cc_products/__version__.py
@@ -1,10 +1,10 @@
 """CC Products."""
 
-__title__ = 'cc_products'
-__description__ = 'Work with Cloud Commerce Products.'
-__url__ = 'http://github.com/axevalley/cc_products.git'
-__version__ = '0.0.1'
-__author__ = 'Luke Shiner'
-__author_email__ = 'luke@lukeshiner.com'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2018 Luke Shiner'
+__title__ = "cc_products"
+__description__ = "Work with Cloud Commerce Products."
+__url__ = "http://github.com/axevalley/cc_products.git"
+__version__ = "0.0.1"
+__author__ = "Luke Shiner"
+__author_email__ = "luke@lukeshiner.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018 Luke Shiner"
