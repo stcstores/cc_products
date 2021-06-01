c0b7b65bc1f6bfb0699d5f2880661c7de5be10cd
Luke Shiner <luke@lukeshiner.com>
2021-06-01 13:52:48 +0100

    Update linters

--- cc_products/__init__.py
+++ cc_products/__init__.py
@@ -4,5 +4,7 @@ CC Products package.
 Provides tools for easiliy working with Cloud Commerce Products.
 """
 
-from .variation import Variation  # NOQA
-from .functions import *  # NOQA
+from .functions import create_range, get_product, get_range
+from .variation import Variation
+
+__all__ = ["get_product", "get_range", "create_range", "Variation"]
